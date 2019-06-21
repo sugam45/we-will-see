73aab643763718f9d357c136d7dc7d1457c6693a
Arkavo Hait <arkavohait@gmail.com>
2019-06-22 03:43:01 +0530

    Initial commit with game board

---
+++ Assets/movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public new Sprite m;
+    public int movement = 3;
+    public Vector2 coord;
+    // Start is called before the first frame update
+    void Start()
+    {
+        coord = m.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
